bfa98085e1c6e05a0c36931b126223d2d57d3b7b
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-08-19 02:21:57 +0530

    AppPlugin fix

--- insight/plugins/AppPlugin.js
+++ insight/plugins/AppPlugin.js
@@ -1,8 +1,3 @@
-import Vue from 'vue';
-import VueLazyload from 'vue-lazyload'
-import Vuex from "vuex";
+
 import Hammer from '@/node_modules/hammerjs/hammer.js';
 // import html2canvas from "html2canvas";
-
-Vue.use(VueLazyload);
-Vue.use(Vuex);
