8aa58ca535b52388a172778c1dac86f0b9eae34e
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-08-20 00:49:02 +0530

    New Login

--- insight/pages/auth/logiin.vue
+++ insight/pages/auth/logiin.vue
@@ -4,27 +4,23 @@
       <div v-if="this.error" class="h-12 rounded-lg w-full px-4 py-2 bg-red-600">
          <p class="font-lato font-bold text-white">Invalid Credentials, try again</p>
       </div>
-      <div class="title font-bold font-muli mt-4 text-green-400">Freaquish</div>
-      <div class="w-auto h-12 mt-8 border rounded-full border-white">
+      <div class="title font-bold font-muli mt-4 text-green-400">freaquish</div>
+      <div class="w-auto h-12 mt-8 border rounded-md border-green-400">
         <i class="fa fa-user-circle mt-4 text-white ml-2" aria-hidden="true"></i>
         <input
         class="outline-none bg-transparent ml-4 text-white font-muli"
         type="text" name="account_id"  placeholder="Phone Number" v-model="account_id">
       </div>
-      <div class="w-auto h-12 mt-4 border rounded-full border-white">
+      <div class="w-auto h-12 mt-4 border rounded-md border-green-400">
         <i class="fa fa-key mt-4 text-white ml-2" aria-hidden="true"></i>
         <input
         class="outline-none bg-transparent ml-4 text-white font-muli"
         type="password" name="Password"  placeholder="Password" v-model="password">
       </div>
       <!-- <div class="text-right mt-4 mr-6 text-gray-600">Forget Password ?</div> -->
-      <div class="w-full h-16 py-2 px-2 mt-10 flex">
-         
-         <button @click="login" class="w-full py-2 mx-5 font-muli font-bold focus:outline-none text-center text-white bg-green-400 rounded-full">Login</button>
-      </div>
-      <div class="ml-8 mr-8 text-center text-gray-200 mt-12 mb-8">
-        <div class="text-md">Don't have any account, join us <br></div>
-        <h1><a class="mt-4 text-green-500 text-lg">REGISTER</a><span class="text-md text-gray-200">, here</span></h1>
+      <div class="w-full h-16 py-2 px-2 mt-10 flex ">
+         <p @click="$router.push('/auth/register')" class="font-lato font-bold text-xl text-white mr-6 mt-3">Register</p>
+         <button @click="login" class="w-full py-2 font-muli font-bold text-white bg-green-400 rounded-lg">Login</button>
       </div>
     </div>
   </div>
