20ba353845f4dc35948be7a1506faa0b919f1e2d
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-08-24 04:52:25 +0530

    VueAdvancedCropper removed from AppPlugin

--- insight/plugins/AppPlugin.js
+++ insight/plugins/AppPlugin.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Hammer from '@/node_modules/hammerjs/hammer.js';
-import { Cropper } from 'vue-advanced-cropper'
+// import { Cropper } from 'vue-advanced-cropper'
 
-Vue.use(Cropper);
+// Vue.use(Cropper);
 // import html2canvas from "html2canvas";
